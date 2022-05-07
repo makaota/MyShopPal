1201cea9be904245a92d2bd8c6e13f33c8d5178a
makaota <74915165+makaota@users.noreply.github.com>
2022-05-07 15:52:35 +0200

    Add App Icon and fix color theme

--- app/src/main/java/com/makaota/myshoppal/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/makaota/myshoppal/ui/activities/DashboardActivity.kt
@@ -10,15 +10,15 @@ import androidx.navigation.ui.setupWithNavController
 import com.makaota.myshoppal.R
 import com.makaota.myshoppal.databinding.ActivityDashboardBinding
 
+/**
+ *  Dashboard Screen of the app.
+ */
 class DashboardActivity : BaseActivity() {
-
-    private lateinit var binding: ActivityDashboardBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
 
-        // Update the background color of the action bar as per our design requirement.
-        // START
         // Update the background color of the action bar as per our design requirement.
         supportActionBar!!.setBackgroundDrawable(
             ContextCompat.getDrawable(
@@ -28,23 +28,21 @@ class DashboardActivity : BaseActivity() {
         )
         // END
 
-        binding = ActivityDashboardBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.navView
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment_activity_dashboard)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_dashboard,
                 R.id.navigation_products,
+                R.id.navigation_dashboard,
                 R.id.navigation_orders,
                 R.id.navigation_sold_products
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
+
         navView.setupWithNavController(navController)
     }
 
